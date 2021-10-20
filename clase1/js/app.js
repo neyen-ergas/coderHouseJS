5cab0aabb4550a65bb5d432814b1596877ac85f8
Kid-N <neyenergas@gmail.com>
2021-10-20 15:31:53 -0300

    clase1

---
+++ clase1/js/app.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Ingrese solo su nombre");
+let apellido = prompt("Ingrese solo su apellido");
+let nacimiento= prompt("Ingrese su año de nacimiento");
+let nombreCompleto = document.write("<p>"+"Hola! "+nombre+" "+apellido+"</p>");
+let edad=document.write("<p>"+"Su edad es: " +(2021-nacimiento)+"</p>");
+
