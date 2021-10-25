35b87ca93f4610e9e993dbef9066015b928d126a
neyen-ergas <76958074+neyen-ergas@users.noreply.github.com>
2021-10-25 16:53:51 -0300

    Add files via upload

---
+++ clase2Ergas/js/app.js
@@ -0,0 +1,7 @@
+let edad = parseInt(prompt("Ingrese su edad"))
+
+if(edad<=18){
+    alert("Usted no posee la edad adecuada para entrar a la pagina")
+}else{
+    document.write("Esta pagina te venderia alcohol por ser +18, pero todavia no sabemos como :(")
+}
